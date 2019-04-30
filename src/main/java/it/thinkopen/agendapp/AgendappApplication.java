b0fe42f8012a99fd938d73ed5d9ecdfcb85603b7
Al1el94 <49283488+Aliel94@users.noreply.github.com>
2019-04-30 16:08:40 +0200

    initial commit

---
+++ src/main/java/it/thinkopen/agendapp/AgendappApplication.java
@@ -0,0 +1,13 @@
+package it.thinkopen.agendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendappApplication.class, args);
+	}
+
+}
