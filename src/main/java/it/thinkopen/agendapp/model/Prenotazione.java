5631a6bdbf3e257fca062af4eb13ff283ffd8e20
Al1el94 <49283488+Aliel94@users.noreply.github.com>
2019-05-02 12:48:35 +0200

    initial commit

--- src/main/java/it/thinkopen/agendapp/model/Prenotazione.java
+++ src/main/java/it/thinkopen/agendapp/model/Prenotazione.java
@@ -1,5 +1,6 @@
 package it.thinkopen.agendapp.model;
 
+import java.util.Date;
 import javax.persistence.*;
 
 import lombok.Data;
@@ -12,6 +13,12 @@ public class Prenotazione {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date oraInizio;
+
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date oraFine;
     
     @ManyToOne
    private Utente utente;
